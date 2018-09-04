1a93398bb0fccade05b33bf5d9bf8f9bacda69b5
RWN <1111@qq.com>
2018-09-04 14:27:05 +0800

    NSobjectCopy

---
+++ CopyExcise/RWNPerson.h
@@ -0,0 +1,13 @@
+//
+//  RWNPerson.h
+//  CopyExcise
+//
+//  Created by shenhua on 2018/9/4.
+//  Copyright © 2018年 RWN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RWNPerson : NSObject
+
+@end
---
+++ CopyExcise/RWNPerson.m
@@ -0,0 +1,13 @@
+//
+//  RWNPerson.m
+//  CopyExcise
+//
+//  Created by shenhua on 2018/9/4.
+//  Copyright © 2018年 RWN. All rights reserved.
+//
+
+#import "RWNPerson.h"
+
+@implementation RWNPerson
+
+@end
